9ef062733980c6211dd34a939c36ad84ca7f01b1
salman saghir <salman.saghir1@gmail.com>
2024-03-05 09:13:00 +0500

    assignment

---
+++ app.js
@@ -0,0 +1,71 @@
+// var num1= +prompt();
+// var num2= +prompt();
+// var opt=prompt();
+// if(opt==="+"){
+//     var result=num1+num2
+//     console.log(result);
+    
+// }
+// else if(opt==="-"){
+//     result=num1-num2
+//     console.log(result);
+
+// }
+// else if(opt==="*"){
+//     result=num1*num2
+//     console.log(result);
+
+// }
+// else if(opt==="/"){
+//     result=num1/num2
+//     console.log(result);
+
+    
+// }
+// else if(opt==="%"){
+//     result=num1%num2
+//     console.log(result);
+
+// }
+// else{
+//     console.log("opretor not correct");
+// }
+// var num1=+prompt("Enter the number");
+// var num2=+prompt("Enter the number");
+// var opretor=prompt("Select Operater");
+
+// if(opretor=="+"){
+//     var result=num1+num2;
+//     console.log(result);
+
+// }
+// else if(opretor=="-"){
+//     var result=num1-num2;
+//     console.log(result);
+
+// }
+// else{
+//     console.log("incorrect operater!!");
+// }
+
+// ------------chapter 9_________
+//Q1:
+// var city=prompt("Entery the City name");
+// if(city =="karachi"){
+// console.log('Welcome to the City of light');
+// }
+
+//Q2:
+var gender=prompt('Enter the gender:');
+if (gender=="male"){
+    console.log('Good Morning Sir.');
+
+    }
+else if(gender=="female")
+{console.log('Good Morning Maam.');
+
+}
+else{
+    console.log('incorrect!');
+}
+
